f709fa223d392d85a4036e79a5946affcbc9d5d4
mehdiskan <mehdiskan@gmail.com>
2023-08-18 13:51:33 +0330

    feat: add a basic chat system

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import { join } from "path"
 import express from "express";
 import socketIO from "socket.io"
+import logger from "morgan"
 
 const PORT = 4000;
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"))
+app.use(logger("dev"))
 app.use(express.static(join(__dirname, "static")))
 app.get("/", (req, res) => res.render("home"))
 
@@ -14,4 +16,22 @@ const handleListening = () =>
     console.log(`🥹 Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
-const io = socketIO.listen(server)
+const io = socketIO(server);
+
+let sockets = []
+
+io.on("connection", (socket) => {
+    socket.on("newMessage", ({ message }) => {
+        socket.broadcast.emit("messageNotif", {
+            message,
+            nickname: socket.nickname || "Anon"
+        })
+    })
+    socket.on("setNickName", ({ nickname }) => {
+        socket.nickname = nickname
+    })
+});
+
+setInterval(() => {
+    console.log(sockets);
+}, 1000);
